f8b6bbe3ac2bdc22f909e084c9755cb697964c30
pragromer1337 <ghostriderjb122@gmail.com>
2024-01-25 10:29:18 +0300

    Выполнили работу с многомерными массивами

---
+++ task3/app.js
@@ -0,0 +1,6 @@
+let brand = [['BMW', 'Lamborghini', 'Bugatti', 'Nissan', 'Toyota'], ['Fila', 'Gucci', 'Prada', 'Adidas', 'Nike'], ['Pepsi', 'Sprite', 'Monster', 'Lays', 'Cheetos'], ['LG', 'Sony', 'Logitech', 'Samsung', 'Huawei']];
+console.log(brand);
+console.log(brand [0] [0]);
+console.log(brand [1] [3]);
+console.log(brand [2] [2]);
+console.log(brand [3] [1]);
